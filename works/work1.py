8f708564e53a3b96001aa901e182b28274d55869
tkyk <tkyk@tkyknoMacBook-Pro.local>
2020-11-06 16:23:53 +0900

    move round

--- works/work1.py
+++ works/work1.py
@@ -6,30 +6,53 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import envs.reach_mz07_env as reach_env
 
-#import gym.envs.robotics.fetch.reach as env_reach
-
 env = reach_env.ReachMz07Env()
-#env = gym.make('FetchReach-v1')
 
-def policy(observation, desired_goal):
-    # Here you would implement your smarter policy. In this case,
-    # we just sample random actions.
-    return env.action_space.sample()
-
-for _ in range(1000):
-    obs = env.reset()
-    done = False
-
-    while not done:
-        env.render()
-        action = policy(obs['observation'], obs['desired_goal'])
+def moveto(action, env):
+    last_pos=np.array([10,0,0])
+    for _ in range(1000):
         obs, reward, done, info = env.step(action)
+        achieved_goal=obs['achieved_goal']
+        dist=np.linalg.norm(achieved_goal-last_pos)
+        if dist<0.0001:
+            break
+        last_pos=achieved_goal
+        env.render()
 
-        # If we want, we can substitute a goal here and re-compute
-        # the reward. For instance, we can just pretend that the desired
-        # goal was what we achieved all along.
-        substitute_goal = obs['achieved_goal'].copy()
-        substitute_reward = env.compute_reward(
-            obs['achieved_goal'], substitute_goal, info)
-        print('reward is {}, substitute_reward is {}'.format(
-            reward, substitute_reward))
+    return (obs, reward, done, info)
+
+
+# pos_ctrl, gripper_ctrl = action[:3], action[3]
+def policy(observation, desired_goal, achieved_goal, step):
+
+    actions=[
+        [0.5,0.5,0.5,0],
+        [-0.5,0.5,0.5,0],
+        [-0.5,-0.5,0.5,0],
+        [0.5,-0.5,0.5,0]
+    ]
+    # action=env.action_space.sample()
+
+    return np.array(actions[step%4])
+
+if __name__=="__main__":
+    for episode in range(100):
+        obs = env.reset()
+        done = False
+        step = 0
+        while not done:
+            step+=1
+            action = policy(obs['observation'], obs['desired_goal'], obs['achieved_goal'], step)
+
+            print(action)
+            obs, reward, done, info = moveto(action, env)
+
+
+            # If we want, we can substitute a goal here and re-compute
+            # the reward. For instance, we can just pretend that the desired
+            # goal was what we achieved all along.
+            substitute_goal = obs['achieved_goal'].copy()
+            substitute_reward = env.compute_reward(
+                obs['achieved_goal'], substitute_goal, info)
+            print('reward is {}, substitute_reward is {}'.format(
+                reward, substitute_reward))
